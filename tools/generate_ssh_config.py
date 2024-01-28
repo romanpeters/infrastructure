b2c3efb876f38f77504471f57d30844a4077dd2b
Roman Peters <mail@romanpeters.nl>
2024-01-28 13:56:48 +0100

    black

--- tools/generate_ssh_config.py
+++ tools/generate_ssh_config.py
@@ -6,11 +6,12 @@ import re
 ANSIBLE_INVENTORY_PATH = "inventory.ini"
 SSH_CONFIG_FILE_PATH = "roles/base/files/ssh/config"
 
+
 def generate_ssh_config(inventory_path, output_path):
-    with open(inventory_path, 'r') as inventory_file:
+    with open(inventory_path, "r") as inventory_file:
         inventory_content = inventory_file.read()
 
-    pattern = re.compile(r'(\S+)\s+ansible_host=(\S+)(?:\s+ansible_user=(\S+))?')
+    pattern = re.compile(r"(\S+)\s+ansible_host=(\S+)(?:\s+ansible_user=(\S+))?")
 
     ssh_config_entries = []
     for match in sorted(pattern.finditer(inventory_content), key=lambda m: m.group(2)):
@@ -23,10 +24,10 @@ def generate_ssh_config(inventory_path, output_path):
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
 
-    with open(output_path, 'w') as output_file:
-        output_file.write('\n\n'.join(ssh_config_entries))
+    with open(output_path, "w") as output_file:
+        output_file.write("\n\n".join(ssh_config_entries))
+
 
 if __name__ == "__main__":
     generate_ssh_config(ANSIBLE_INVENTORY_PATH, SSH_CONFIG_FILE_PATH)
     print(f"SSH config file generated at {SSH_CONFIG_FILE_PATH}")
-
