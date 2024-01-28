36df0dafa4431509c5040e2e44547e8452fc12d5
Roman <mail@romanpeters.nl>
2024-01-28 14:50:39 +0100

    Dev (#1)
    * pipelining
    
    * changed_when: false
    
    * run_once
    
    * encrypt personal info
    
    * gitconfig
    
    * strategy var
    
    * ansible-lint
    
    * #python3
    
    * black
    
    * git configs

--- tools/generate_hosts_file.py
+++ tools/generate_hosts_file.py
@@ -1,14 +1,17 @@
+#!/usr/bin/env python3
+
 import os
 import re
 
 ANSIBLE_INVENTORY_PATH = "inventory.ini"
 ANSIBLE_HOSTS_FILE_PATH = "roles/base/files/etc/hosts"
 
+
 def generate_hosts_file(inventory_path, output_path):
-    with open(inventory_path, 'r') as inventory_file:
+    with open(inventory_path, "r") as inventory_file:
         inventory_content = inventory_file.read()
 
-    pattern = re.compile(r'(\S+)\s+ansible_host=(\S+)')
+    pattern = re.compile(r"(\S+)\s+ansible_host=(\S+)")
 
     hosts = []
     for match in sorted(pattern.finditer(inventory_content), key=lambda m: m.group(2)):
@@ -20,10 +23,10 @@ def generate_hosts_file(inventory_path, output_path):
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
 
-    with open(output_path, 'w') as output_file:
-        output_file.write('\n'.join(hosts))
+    with open(output_path, "w") as output_file:
+        output_file.write("\n".join(hosts))
+
 
 if __name__ == "__main__":
     generate_hosts_file(ANSIBLE_INVENTORY_PATH, ANSIBLE_HOSTS_FILE_PATH)
     print(f"Hosts file generated at {ANSIBLE_HOSTS_FILE_PATH}")
-
